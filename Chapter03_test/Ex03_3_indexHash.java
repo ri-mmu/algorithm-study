a8d82dbc1e2f17574a41f6e2ab3a69720be6bec7
rim <kkjjrr0504@gmail.com>
2025-09-15 22:32:10 +0900

    feat: 03-3 인덱스에 의미 부여하여 불어보기

---
+++ Chapter03_test/Ex03_3_indexHash.java
@@ -0,0 +1,28 @@
+package Chapter03_test;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Ex03_3_indexHash {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int[] count = new int[1001];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int number = Integer.parseInt(st.nextToken());
+            count[number]++; // 처음 값이 0이니까 count[number] += 1 이랑 같은 뜻
+        }
+        br.close();
+        for (int i = 0; i <= 1000; i++) {
+            if (count[i] != 0) {
+                for (int j = 0; j < count[i]; j++) {
+                    bw.write(i + " ");
+                }
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
+}
