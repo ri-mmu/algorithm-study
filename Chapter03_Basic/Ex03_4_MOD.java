0449eaf26be62a575eb85152f1a9b1e17340b90e
rim <kkjjrr0504@gmail.com>
2025-09-15 22:48:51 +0900

    feat: 나머지 연산 분배법칙 활용 코드

---
+++ Chapter03_Basic/Ex03_4_MOD.java
@@ -0,0 +1,11 @@
+package Chapter03_Basic;
+
+public class Ex03_4_MOD {
+    public static void main(String[] arg) throws Exception {
+        long answer = 1;
+        for (int i = 1; i <= 50; i++) {
+            answer = (answer * i) % 10007;
+        }
+        System.out.println(answer % 10007);
+    }
+}
