91db2e86526086427a6e7bfb0b0e0b828536b0ca
rim <kkjjrr0504@gmail.com>
2025-09-15 18:40:17 +0900

    feat: 03-2 버퍼를 사용한 입출력 코드

---
+++ Chapter03_test/Ex03_2.java
@@ -0,0 +1,18 @@
+package Chapter03_test;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Ex03_2 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a);
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int b = Integer.parseInt(br.readLine());
+        bw.write(String.valueOf(b));
+        bw.flush();
+    }
+}
