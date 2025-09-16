67e30ab0cb54f6e1c05a7f64815f15a0b4785df8
rim <kkjjrr0504@gmail.com>
2025-09-16 15:37:15 +0900

    solve: P11729 숫자의 합

---
+++ Chapter04_DS/P11720_숫자의합.java
@@ -0,0 +1,22 @@
+package Chapter04_DS;
+
+import java.util.*;
+
+// BOJ 1920: 수 찾기
+// 아이디어: 문자열 -> 문자 배열 -> 숫자
+// 시간복잡도: N
+
+public class P11720_숫자의합 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        // 입력값을 String형 변수 sNum에 저장한 후 char[]형 변수로 변환하기
+        String sNum = sc.next();
+        char[] cNum = sNum.toCharArray();
+        int sum = 0;
+        for (int i = 0; i < cNum.length; i++) {
+            sum += cNum[i] - '0';
+        }
+        System.out.print(sum);
+    }
+}
