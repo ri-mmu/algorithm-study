15e268b49b6fe8677e8bacc2cb6a18f53ca25ca9
rim <kkjjrr0504@gmail.com>
2025-09-16 17:10:58 +0900

    solve: PB1546 평균 구하기

---
+++ Chapter04_DS/P02_B1546_평균.java
@@ -0,0 +1,26 @@
+package Chapter04_DS;
+
+// BOJ 1920: 수 찾기
+// 아이디어: 결합법칙
+// 시간복잡도: N
+
+import java.util.*;
+
+public class P02_B1546_평균 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+        long max = 0;
+        long sum = 0;
+        for (int i = 0; i < N; i++) {
+            if (max < A[i]) max = A[i];
+            sum += A[i];
+        }
+        System.out.println((float)sum / max * 100 / N);
+    }
+}
+
