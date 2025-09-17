522143e9a577bfd84caa65049be11aba79ab7638
rim <kkjjrr0504@gmail.com>
2025-09-17 20:26:04 +0900

    solve: B11659 구간 합 구하기

---
+++ Chapter04_DS/P03_B11659_구간합구하기.java
@@ -0,0 +1,28 @@
+package Chapter04_DS;
+
+// BOJ 11659: 수 찾기
+// 아이디어: 구간의 합 배열 저장
+// 시간복잡도: 1
+
+import java.io.*;
+import java.util.*;
+
+public class P03_B11659_구간합구하기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int suNo = Integer.parseInt(stringTokenizer.nextToken());
+        int quizNo = Integer.parseInt(stringTokenizer.nextToken());
+        long[] S = new long[suNo + 1];
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        for (int i = 1; i <= suNo; i++) {
+            S[i] = S[i - 1] + Integer.parseInt(stringTokenizer.nextToken());
+        }
+        for (int q = 0; q < quizNo; q++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int i = Integer.parseInt(stringTokenizer.nextToken());
+            int j = Integer.parseInt(stringTokenizer.nextToken());
+            System.out.println(S[j] - S[i - 1]);
+        }
+    }
+}
