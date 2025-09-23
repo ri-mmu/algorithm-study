4666df120cc4e70c708aa3efb1095aa201318ceb
rim <kkjjrr0504@gmail.com>
2025-09-23 16:11:40 +0900

    solve: B1253 최솟값 찾기

---
+++ Chapter04_DS/P10_B11003_최솟값.java
@@ -0,0 +1,43 @@
+package Chapter04_DS;
+
+// 아이디어: 슬라이딩 윈도우
+// 시간복잡도: n
+
+import java.io.*;
+import java.util.*;
+
+public class P10_B11003_최솟값 {
+    public static final Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        Deque<Node> mydeque = new LinkedList<>();
+        for (int i = 0; i < N; i++) {
+            int now = Integer.parseInt(st.nextToken());
+            while (!mydeque.isEmpty() && mydeque.getLast().value > now) {
+                mydeque.removeLast();
+            }
+            mydeque.addLast(new Node(now, i));
+            if (mydeque.getFirst().index <= i - L) {
+                mydeque.removeFirst();
+            }
+            bw.write(mydeque.getFirst().value + " ");
+        }
+        bw.flush();
+        br.close();
+    }
+
+    static class Node {
+        public int value;
+        public int index;
+
+        public Node(int value, int index) {
+            this.value = value;
+            this.index = index;
+        }
+    }
+}
